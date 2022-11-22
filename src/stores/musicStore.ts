bf4e5a0f34a351582637fb1a76ba903f3b27ce2d
moritzheine <44192283+Moritzheine@users.noreply.github.com>
2022-11-22 17:24:21 +0100

    eslint

--- src/stores/musicStore.ts
+++ src/stores/musicStore.ts
@@ -1,70 +1,54 @@
 import { makeAutoObservable } from "mobx";
 import Track from "../models/track/track";
-import { playlist } from "../constants/playlist";
-import { getStoreFromContext } from "../helpers";
-import { SpotifyStore } from "./spotifyStore";
-import { unescape } from "querystring";
 
 
 export class MusicStore {
-  
   private _trackList: Track[] = [];
   
   constructor() {
     makeAutoObservable(this);
-    this.loadMockPlaylist();
-    // this.getToken()
   }
 
-  loadMockPlaylist() {
-    playlist.split("#").forEach((val) => {
-      const lines = val.split("\n");
-      const metaData = lines[0];
-      const path = lines[1];
-      // const track = new RekordboxTrack({ header: metaData, filePath: path });
-      // this.trackList.push(track);
-    });
-  }
 
   loadCollectionXml(file: File) {
     const parser = new DOMParser();
-    const reader = new FileReader()
+    const reader = new FileReader();
 
     reader.onload = async (e) => {
-      if (e.target === null) return
-      const text = (e.target.result)?.toString() ?? ""
+      if (e.target === null) return;
+      const text = (e.target.result)?.toString() ?? "";
       const xml = parser.parseFromString(text, "text/xml");
       const collection = xml.getElementsByTagName("COLLECTION")[0];
       // console.log(collection)
       // setTrackNumber(collection.attributes.getNamedItem("Entries")?.textContent)
-      const tracks = Array.from(collection.getElementsByTagName("TRACK"))
-      console.log(tracks[0].getAttributeNames())
+      const tracks = Array.from(collection.getElementsByTagName("TRACK"));
+      console.log(tracks[0].getAttributeNames());
       this.trackList = tracks.map((track) => {
         return new Track({
-          Name: track.getAttribute("Name") ?? "",
-          Artist: track.getAttribute("Artist"),
-          TotalTime: track.getAttribute("TotalTime"),
-          Location: decodeURIComponent(track.getAttribute("Location")?.replace("file://localhost", "") ?? ""),
+          Name      : track.getAttribute("Name") ?? "",
+          Artist    : track.getAttribute("Artist"),
+          TotalTime : track.getAttribute("TotalTime"),
+          Location  : decodeURIComponent(track.getAttribute("Location")?.replace("file://localhost", "") ?? ""),
         });
-      })
+      });
     };
-    reader.readAsText(file)
+    reader.readAsText(file);
   }
 
   generatePlaylist(fileName: string, tracks: Track[]) {
     // Download it
     const data = "#EXTM3U" + tracks.map(track => 
       track.toM3U8()
-    ).join("")
+    ).join("");
     // console.log(data)
     const blob = new Blob([data]);
     const fileDownloadUrl = URL.createObjectURL(blob);
 
     // Create blob link to download
-    const link = document.createElement('a');
+    const link = document.createElement("a");
     link.href = fileDownloadUrl;
     link.setAttribute(
-      'download',
+      "download",
       fileName+".m3u8",
     );
 
